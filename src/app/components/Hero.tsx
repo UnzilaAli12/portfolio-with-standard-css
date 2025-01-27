3d5829c9fe95afba5702033364b74baa0a3e5d87
UnzilaAli12 <unzila.aliasghar@gmail.com>
2025-01-27 17:49:57 +0500

    Fixed ESLint errors and updated code

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from '@/app/About/about.module.css';
 import Link from 'next/link';
+import Image from 'next/image'
 
 
 
@@ -11,11 +12,11 @@ const Hero = () => {
     <div className='container' >
     <h1>Unzila Ali </h1>
   
-<img src="1.jfif" alt="profile picture" width={200} height={200} /> 
+<Image src="/1.jfif" alt="Profile Picture" width={200} height={200}/>
   <h2>Frontend Developer and Graphic Designer </h2> 
   
   <p>
-  I'm someone who likes setting goals and can handle lots of tasks at once. I can adapt to different situations and I'm good at talking through problems and finding solutions. I learn quickly and being outgoing helps me work well in teams. I can guide and lead people by understanding their points of view. In my studies in International Relations, I've learned about global trends, which gives me an edge in understanding what's happening worldwide. On top of that, I know graphic design, so I can help businesses make their visuals look great. I'm a friendly team player who can bring a mix of learning and practical skills to get things done effectively.
+  I am someone who likes setting goals and can handle lots of tasks at once. I can adapt to different situations and I am good at talking through problems and finding solutions. I learn quickly and being outgoing helps me work well in teams. I can guide and lead people by understanding their points of view. In my studies in International Relations, I have learned about global trends, which gives me an edge in understanding what is happening worldwide. On top of that, I know graphic design, so I can help businesses make their visuals look great. I am a friendly team player who can bring a mix of learning and practical skills to get things done effectively.
 </p> 
   </div>
   <div className={styles.button} >
